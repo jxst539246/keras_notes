de05bcb415b964071c3e9cbd5c37534a00fc7ddb
mr <809505910@qq.com>
2017-04-13 15:54:19 +0800

    finish mnist

--- mnist.py
+++ mnist.py
@@ -7,6 +7,44 @@ from keras.layers.core import Dense,Dropout,Activation
 from keras.utils import np_utils
 
 num_classes = 10
-(X_train,Y_train),(X_test,Y_test) = mnist.load_data()
-print(X_train.shape)
+(X_train,y_train),(X_test,y_test) = mnist.load_data()
+
+
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+Y_train = np_utils.to_categorical(y_train,num_classes)
+Y_test = np_utils.to_categorical(y_test,num_classes)
+
+model = Sequential()
+model.add(Dense(512,input_shape=(784,),activation='relu',))
+model.add(Dropout(0.2))
+model.add(Dense(512,activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(10,activation='softmax'))
 print(Y_train.shape)
+print(Y_test.shape)
+
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+print(model.summary())
+
+model.fit(X_train,Y_train,batch_size=128,nb_epoch=5,verbose=1,validation_data=(X_test,Y_test))
+score = model.evaluate(X_test,Y_test,show_accuracy=True)
+
+print(score)
+predicted_classes = model.predict_classes(X_test)
+
+# Check which items we got right / wrong
+correct_indices = np.nonzero(predicted_classes == y_test)[0]
+incorrect_indices = np.nonzero(predicted_classes != y_test)[0]
+
+plt.figure()
+for i, incorrect in enumerate(incorrect_indices[:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_test[incorrect].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[incorrect], y_test[incorrect]))
+
+plt.show()
